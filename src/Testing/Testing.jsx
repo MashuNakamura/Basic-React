81eacb01fc98ad3096f3b102e0637d57a682529e
Okerachuchupurin <145318409+MashuNakamura@users.noreply.github.com>
2025-04-13 15:59:51 +0700

    Change to a Doc

---
+++ src/Testing/Testing.jsx
@@ -0,0 +1,63 @@
+export default function Testing(){
+    const Spread_syntax = {
+        text: "Text from Spread Syntax"
+    }
+
+    return (
+        <div>
+            <DefaultJSX />
+            <ExampleJS />
+            <ExampleJSWithCss />
+            <Props text="Props"/>
+            <SpreadSyntax {...Spread_syntax}/>
+        </div>
+    );
+}
+
+// Default Function JSX
+export function DefaultJSX(){
+    return (
+    <h1>Default JSX</h1>
+    )
+}
+
+// Example JS usage
+export function ExampleJS(){
+    const text = "Example JS";
+    return (
+        <h1>{text.toLowerCase()}</h1>
+    )
+}
+
+// Example JS with CSS using {{ }}
+export function ExampleJSWithCss(){
+    const test = "upper this"
+    return (
+        <h1 style = {{
+            color: "red",
+            background: "blue"
+        }}>
+            {test.toUpperCase()}
+        </h1>
+    )
+}
+
+// Example Props, if no input text at default function, then print "Aduh Kocak"
+export function Props({text = "Aduh Kocak"}){
+    const style = {
+        color: "blue",
+        background: "red"
+    }
+    return (
+        <h1 style={style}>
+            {text}
+        </h1>
+    )
+}
+
+// Example SpreadSyntax (const from default function)
+export function SpreadSyntax({text}){
+    return (
+        <h1>{text}</h1>
+    )
+}
--- src/Testing/main.jsx
+++ src/Testing/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
-import HelloWorld from "./HelloWorld";
 import { StrictMode } from "react";
+import Testing from "./Testing.jsx";
 
 createRoot(document.getElementById("root"))
 .render(
     <StrictMode>
-        <HelloWorld />
+        <Testing />
     </StrictMode>
 )
